8cc7923e80eb33fd4034b9b80d256edd2e09ede9
jhoniernem <jhonny30ca@gmail.com>
2022-11-18 08:31:42 -0500

    07

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 console.log('Hola Mundo');
-require('dotenv').config();
 const server = require('./app');
 const { conection } = require('./db');
 const {upCitiesApi} = require('./controllers/cities/index')
 const {UploadProperties,UploadUsers} = require('./controllers/others/uploadData')
 
-const {
-  PORT
-} = process.env;
-
 const force = true
 
 // Syncing all the models at once.
 conection.sync({ force:force }).then(() => {
-  server.listen(PORT || 5000, async() => {
+  server.listen(5000, async() => {
     await upCitiesApi()
     force && UploadProperties()
     force && UploadUsers()
-    console.log(`%s listening at ${PORT || 5000}`); 
+    console.log(`%s listening at ${5000}`); 
     });
 });
