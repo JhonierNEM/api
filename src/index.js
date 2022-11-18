cb51be213c667ea96ee1fc4c94f351f5a9c329b3
jhoniernem <jhonny30ca@gmail.com>
2022-11-18 00:06:29 -0500

    02

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('Hola Mundo');
+const server = require('./app');
+const { conection } = require('./db');
+const {upCitiesApi} = require('./ccontrollers/cities/index')
+const {UploadProperties,UploadUsers} = require('./controllers/others/uploadData')
+
+const force = true
+
+// Syncing all the models at once.
+conection.sync({ force:force }).then(() => {
+  server.listen(3001, async() => {
+    await upCitiesApi()
+    force && UploadProperties()
+    force && UploadUsers()
+    console.log('%s listening at 3001'); 
+    });
+});
