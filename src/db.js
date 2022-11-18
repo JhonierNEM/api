bd5db530b58e9b95b6a880f5abf55e06928c54a7
jhoniernem <jhonny30ca@gmail.com>
2022-11-17 23:39:09 -0500

    updated

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  PGUSER,PGPASSWORD, PGHOST, PGPORT,PGDATABASE
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/propertiesyou`, {
+const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
   logging: false, 
   native: false, 
 });
